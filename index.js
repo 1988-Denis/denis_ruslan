f6f4ebb0b2f0a0d3c926bd6ca4f60d8f104953fc
Denis <deshatyrko@yandex.ru>
2025-03-16 20:24:29 +0300

    Добавил задание по Условиям FizzBuzz

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+const chislo = Math.floor(Math.random()*(15-3+1))+3;
+
+fizz = chislo/3;
+buzz = chislo/5;
+
+if (Number.isInteger(fizz)) {
+    console.log('Fizz');
+} else if (Number.isInteger(buzz)) {
+    console.log('Buzz');
+} else {
+    console.log('FizzBuzz');
+}
