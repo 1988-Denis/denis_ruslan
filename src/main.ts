185fbaa964901cba9fcc1abb67c91f22cf6fc604
Denis <deshatyrko@yandex.ru>
2025-04-14 20:33:28 +0300

    Добавлено задание Укажите типы

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,29 @@
-console.log(30);
+type A = {
+  age: number | string;
+  nick: string;
+  info: number[] | { id: number } | string[] | {};
+  photo: string | null;
+};
+const a1: A = {
+  age: 10,
+  nick: 'mynick',
+  info: [1, 100],
+  photo: 'string',
+};
 
+const a2: A = {
+  age: '10 years',
+  nick: '',
+  info: { id: 100 },
+  photo: null,
+};
+
+const a3: A = {
+  nick: '       ',
+  info: ['secret', 'key'],
+};
+
+const a4: A = {
+  nick: '',
+  info: {},
+};
