b8e36a5f7191080370fa36679e79738019d86239
Denis <deshatyrko@yandex.ru>
2025-04-17 20:47:46 +0300

    Задание Цвета

--- src/main.ts
+++ src/main.ts
@@ -143,22 +143,33 @@
 //   createdAtISO: new Date().toISOString(),
 // };
 // console.log(myItems);
+//
+// import dayjs from 'dayjs';
+//
+// console.log(12, dayjs('2024-08-23T12:30:00Z').toISOString());
+// console.log(2, dayjs('2024-08-23T15:30:00+03:00').toISOString());
+// console.log(5, dayjs('2024-08-24T01:30:00+13:00').toISOString());
+// console.log(9, dayjs('2024-08-23T09:30:00-03:00').toISOString());
+// console.log('----\n');
+//
+// console.log(4, dayjs('2024-08-23T15:30:00Z').toISOString());
+// console.log(6, dayjs('2024-08-23T17:30:00+02:00').toISOString());
+// console.log(10, dayjs('2024-08-23T14:30:00-01:00').toISOString());
+// console.log(8, dayjs('2024-08-23T03:30:00-12:00').toISOString());
+// console.log('----\n');
+//
+// console.log(1, dayjs('2024-08-23T03:30:00-11:00').toISOString());
+// console.log(3, dayjs('2024-08-23T14:30:00Z').toISOString());
+// console.log(7, dayjs('2024-08-23T10:30:00-04:00').toISOString());
+// console.log(11, dayjs('2024-08-24T02:30:00+12:00').toISOString());
 
-import dayjs from 'dayjs';
+import chalk from 'chalk';
 
-console.log(12, dayjs('2024-08-23T12:30:00Z').toISOString());
-console.log(2, dayjs('2024-08-23T15:30:00+03:00').toISOString());
-console.log(5, dayjs('2024-08-24T01:30:00+13:00').toISOString());
-console.log(9, dayjs('2024-08-23T09:30:00-03:00').toISOString());
-console.log('----\n');
+enum Street {
+  Green = 'Green',
+  Lenina = 'Red',
+  Jykovan = 'Yellow',
+}
+const streets = Object.values(Street);
 
-console.log(4, dayjs('2024-08-23T15:30:00Z').toISOString());
-console.log(6, dayjs('2024-08-23T17:30:00+02:00').toISOString());
-console.log(10, dayjs('2024-08-23T14:30:00-01:00').toISOString());
-console.log(8, dayjs('2024-08-23T03:30:00-12:00').toISOString());
-console.log('----\n');
-
-console.log(1, dayjs('2024-08-23T03:30:00-11:00').toISOString());
-console.log(3, dayjs('2024-08-23T14:30:00Z').toISOString());
-console.log(7, dayjs('2024-08-23T10:30:00-04:00').toISOString());
-console.log(11, dayjs('2024-08-24T02:30:00+12:00').toISOString());
+console.log(chalk.green.bold(streets[0]), chalk.red.bold(streets[1]), chalk.yellow.bold(streets[2]));
